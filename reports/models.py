dec2a3f316e1a5ce70086758c3c64cd3001218f8
Marcelo Theodoro <marcelotheodoro@outlook.com>
2019-07-21 23:36:06 -0300

    Create Report model

--- reports/models.py
+++ reports/models.py
@@ -1,1 +1,111 @@
-# TODO
+import datetime
+import itertools
+import locale
+from decimal import Decimal
+
+from django.conf import settings
+from django.db import models
+
+
+def next_minutes_generator(start_datetime):
+    return (
+        start_datetime + datetime.timedelta(seconds=i)
+        for i in itertools.count(start=60, step=60)
+    )  # pragma: no cover
+
+
+class Report(models.Model):
+
+    subscriber = models.CharField(max_length=11, db_index=True)
+    destination = models.CharField(max_length=11)
+
+    call_started_at = models.DateTimeField()
+    call_ended_at = models.DateTimeField()
+
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def save(self, *args, **kwargs):
+        self.price = self.calculate_price()
+        super().save(*args, **kwargs)
+
+    def calculate_price(self):
+        minutes = self.minutes_in_each_tarrif()
+        per_minute_charge = self.per_minute_charge(minutes)
+        charge = self.charge()
+        return charge + per_minute_charge
+
+    def charge(self):
+        if (
+            self.standard_starts_at.hour
+            <= self.call_started_at.hour
+            <= self.standard_ends_at.hour
+        ):
+            return self.standard_charge
+        return self.reduced_charge
+
+    def per_minute_charge(self, minutes):
+        per_minute_charge = minutes["reduced"] * self.reduced_charge_per_minute
+        per_minute_charge += minutes["standard"] * self.standard_charge_per_minute
+        return per_minute_charge
+
+    def minutes_in_each_tarrif(self):
+        count_standard = 0
+        count_reduced = 0
+        for moment in next_minutes_generator(self.call_started_at):  # pragma: no branch
+            if moment > self.call_ended_at:
+                break
+            if (
+                self.standard_starts_at.hour
+                <= moment.hour
+                <= self.standard_ends_at.hour
+            ):
+                count_standard += 1
+            else:
+                count_reduced += 1
+        return {"reduced": count_reduced, "standard": count_standard}
+
+    @property
+    def price_label(self):
+        price = self.price.quantize(Decimal("0.01"))
+        locale.setlocale(locale.LC_MONETARY, "pt_BR.UTF-8'")
+        return locale.currency(price, grouping=True)
+
+    @property
+    def call_start_date(self):
+        return self.call_started_at.date()
+
+    @property
+    def call_start_time(self):
+        return self.call_started_at.time()
+
+    @property
+    def call_duration(self):
+        duration = self.call_ended_at - self.call_started_at
+        total_seconds = duration.total_seconds()
+        hours, minutes = divmod(total_seconds, 3600)
+        minutes, seconds = divmod(minutes, 60)
+        return f"{int(hours)}h{int(minutes)}m{int(seconds)}s"
+
+    @property
+    def standard_starts_at(self):
+        return settings.FEES["standard"]["starts_at"]
+
+    @property
+    def standard_ends_at(self):
+        return settings.FEES["standard"]["ends_at"]
+
+    @property
+    def standard_charge(self):
+        return settings.FEES["standard"]["charge"]
+
+    @property
+    def standard_charge_per_minute(self):
+        return settings.FEES["standard"]["charge_per_minute"]
+
+    @property
+    def reduced_charge(self):
+        return settings.FEES["reduced"]["charge"]
+
+    @property
+    def reduced_charge_per_minute(self):
+        return settings.FEES["reduced"]["charge_per_minute"]
---
+++ reports/tests/test_models.py
@@ -0,0 +1,81 @@
+from copy import copy
+
+from dateutil.parser import parse
+from django.test import TestCase
+
+from ..models import Report
+
+
+class ReportTestCase(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.standard_data = {
+            "subscriber": "11981094379",
+            "destination": "1198109437",
+            "call_started_at": parse("2016-01-01T15:00:01Z"),
+            "call_ended_at": parse("2016-01-01T15:50:37Z"),
+        }
+        cls.reduced_data = {
+            "subscriber": "11981094379",
+            "destination": "1198109437",
+            "call_started_at": parse("2016-01-01T22:00:00Z"),
+            "call_ended_at": parse("2016-01-02T05:59:59Z"),
+        }
+        cls.standard_and_reduced_data = {
+            "subscriber": "11981094379",
+            "destination": "1198109437",
+            "call_started_at": parse("2016-01-01T21:57:13Z"),
+            "call_ended_at": parse("2016-01-01T22:17:53Z"),
+        }
+
+        cls.report_standard_only = Report.objects.create(**cls.standard_data)
+        cls.report_reduced_only = Report.objects.create(**cls.reduced_data)
+        cls.report_standard_and_reduced = Report.objects.create(
+            **cls.standard_and_reduced_data
+        )
+
+    def test_call_price_standard_only(self):
+        self.assertEqual(self.report_standard_only.price_label, "R$ 4,86")
+
+    def test_call_price_reduced_only(self):
+        self.assertEqual(self.report_reduced_only.price_label, "R$ 0,36")
+
+    def test_call_price_standard_and_reduced(self):
+        self.assertEqual(self.report_standard_and_reduced.price_label, "R$ 0,54")
+
+    def test_call_price_over_day(self):
+        # calc = ((16*60) * 0.09) + 0.36
+        data = copy(self.standard_data)
+        data["call_ended_at"] = parse("2016-01-02T15:00:01Z")
+        report = Report.objects.create(**data)
+        self.assertEqual(report.price_label, "R$ 86,76")
+
+    def test_call_price_less_than_one_minute(self):
+        data = copy(self.standard_data)
+        data["call_ended_at"] = parse("2016-01-01T15:00:59Z")
+        report = Report.objects.create(**data)
+        self.assertEqual(report.price_label, "R$ 0,36")
+
+    def test_call_price_two_hours(self):
+        data = copy(self.standard_data)
+        data["call_ended_at"] = parse("2016-01-01T17:00:01Z")
+        report = Report.objects.create(**data)
+        self.assertEqual(report.price_label, "R$ 11,16")
+
+    def test_call_start_date(self):
+        self.assertEqual(
+            self.report_standard_only.call_start_date,
+            self.standard_data["call_started_at"].date(),
+        )
+
+    def test_call_start_time(self):
+        self.assertEqual(
+            self.report_standard_only.call_start_time,
+            self.standard_data["call_started_at"].time(),
+        )
+
+    def test_call_duration(self):
+        self.assertEqual(self.report_standard_only.call_duration, "0h50m36s")
+        self.assertEqual(self.report_reduced_only.call_duration, "7h59m59s")
+        self.assertEqual(self.report_standard_and_reduced.call_duration, "0h20m40s")
